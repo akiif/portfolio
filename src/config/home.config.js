3f191ab9b13cacf66790c2e570c50148f221235c
Akif Mohammed <akiif.dev@gmail.com>
2024-08-17 00:13:07 +0530

    feat(link): Updated the resume link.

--- src/config/home.config.js
+++ src/config/home.config.js
@@ -1,6 +1,6 @@
 const homeConfig = {
   resumeLink:
-    'https://drive.google.com/u/2/uc?id=1aKRYWecb0zwVE2q58_vL6D0qH7tgqc1x&export=download',
+    'https://drive.google.com/uc?export=download&id=13wg4O8rIj8UqfbNXqBXVLfA2Wu5R5aB9',
   contactLink: 'mailto:akiif.dev@gmail.com?subject=Get In Touch&body=Hey Akif,',
 };
 
